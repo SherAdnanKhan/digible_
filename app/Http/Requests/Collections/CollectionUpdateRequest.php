132d1b0219d44078792998e8206cba4bbf949f9d
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 13:56:01 +0500

    removed auth from request and add status to rules using model constant

--- app/Http/Requests/Collections/CollectionUpdateRequest.php
+++ app/Http/Requests/Collections/CollectionUpdateRequest.php
@@ -2,28 +2,24 @@
 
 namespace App\Http\Requests\Collections;
 
+use App\Models\Collection;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class CollectionUpdateRequest extends FormRequest {
- /**
-  * Determine if the user is authorized to make this request.
-  *
-  * @return bool
-  */
- public function authorize() {
-  return true;
- }
+class CollectionUpdateRequest extends FormRequest
+{
 
- /**
-  * Get the validation rules that apply to the request.
-  *
-  * @return array
-  */
- public function rules() {
-  return [
-   'name' => 'required|string|max:255|unique:collections',
-   'image' => 'nullable|mimes:jpg,jpeg,png,gif|max:20000',
-   'status' => 'in:Pending,Approved,Rejected',
-  ];
- }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255|unique:collections',
+            'image' => 'nullable|mimes:jpg,jpeg,png,gif|max:20000',
+            'status' => ['string', 'max:255', Rule::in(array_keys(Collection::statuses()))],
+        ];
+    }
 }

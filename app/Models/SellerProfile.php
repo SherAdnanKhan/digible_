aeb05b1a3b35bb778765c048d5078bc1763c9f42
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 19:45:11 +0500

    Seller model is update add const for type and status

---
+++ app/Models/SellerProfile.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SellerProfile extends Model
+{
+    use HasFactory;
+    const SELLER_TYPE_INDIVIDUAL = 'individual';
+    const SELLER_TYPE_CARDHOUSE = 'cardhouse';
+    const SELLER_TYPE_DIGI = 'digi';
+    const STATUS_PENDING = "pending";
+    const STATUS_APPROVED = "approved";
+    const STATUS_REJECTED = "rejected";
+
+    protected $guarded = ['created_at'];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public static function statuses(): array
+    {
+        return [
+            static::STATUS_PENDING => "pending",
+            static::STATUS_APPROVED => "approved",
+            static::STATUS_REJECTED => "rejected",
+        ];
+    }
+    public static function typees(): array
+    {
+        return [
+            static::SELLER_TYPE_INDIVIDUAL => "individual",
+            static::SELLER_TYPE_CARDHOUSE => "cardhouse",
+            static::SELLER_TYPE_DIGI => "digi",
+        ];
+    }
+}

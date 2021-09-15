5a6af0b96c5c76109de987596d3c0d6f1e68acbb
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 16:17:05 +0500

    add trasnfromer for the Collection item

---
+++ app/Http/Transformers/Collections/CollectionItemTransformer.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Transformers\Collections;
+
+use App\Models\CollectionItem;
+use App\Http\Transformers\BaseTransformer;
+use App\Http\Transformers\Collections\ItemTypeTransformer;
+use App\Http\Transformers\Collections\CollectionTransformer;
+
+class CollectionItemTransformer extends BaseTransformer
+{
+    protected $defaultIncludes = [
+        'user', 'collection',
+    ];
+
+    public function transform(CollectionItem $collectionItem)
+    {
+        return [
+            'id' => $collectionItem->id,
+            'name' => $collectionItem->name,
+            'label' => $collectionItem->label,
+            'physical' => $collectionItem->physical,
+            'image' => $collectionItem->image,
+            'description' => $collectionItem->description,
+            'edition' => $collectionItem->edition,
+            'graded' => $collectionItem->graded,
+            'year' => $collectionItem->year,
+            'population' => $collectionItem->population,
+            'publisher' => $collectionItem->publisher,
+            'status' => $collectionItem->status,
+            'created_at' => $collectionItem->created_at,
+            'updated_at' => $collectionItem->updated_at,
+
+        ];
+    }
+
+    public function includeCollection(CollectionItem $collectionItem)
+    {
+        $collection = $collectionItem->collection;
+        return $this->item($collection, new CollectionTransformer);
+    }
+
+    public function includeUser(CollectionItem $collectionItem)
+    {
+        $collectionItemType = $collectionItem->collectionItemType;
+        return $this->item($collectionItemType, new ItemTypeTransformer);
+    }
+
+}

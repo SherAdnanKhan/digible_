6064c486fbc48a4d4da6b906d328cfba689645e1
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 14:03:02 +0500

    rename methods and add try catch in itemtypeservice

--- app/Http/Services/Collections/ItemTypeService.php
+++ app/Http/Services/Collections/ItemTypeService.php
@@ -1,63 +1,54 @@
 <?php
 namespace App\Http\Services\Collections;
 
+use App\Exceptions\ErrorException;
 use App\Http\Repositories\Collections\ItemTypeRepository;
 use App\Http\Services\BaseService;
+use App\Models\CollectionItemType;
 use Exception;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
-use InvalidArgumentException;
 
-class ItemTypeService extends BaseService {
- protected $repository;
+class ItemTypeService extends BaseService
+{
+    protected $repository;
 
- public function __construct(ItemTypeRepository $repository) {
-  $this->repository = $repository;
- }
+    public function __construct(ItemTypeRepository $repository)
+    {
+        $this->repository = $repository;
+    }
 
- public function getAll() {
-  Log::info(__METHOD__ . " -- Collection item type data all fetched: ");
-  return $this->repository->getAll();
- }
+    public function getAll()
+    {
+        Log::info(__METHOD__ . " -- Collection item type data all fetched: ");
+        return $this->repository->getAll();
+    }
 
- public function getById($id) {
-  Log::info(__METHOD__ . " -- Collection item type data fetched ");
-  return $this->repository->getById($id);
- }
+    public function save(array $data): void
+    {
+        try {
+            $newData['name'] = $data['name'];
+            $newData['label'] = Arr::exists($data, 'label') ? $data['label'] : null;
 
- public function saveItemType(array $data): void {
-  Log::info(__METHOD__ . " -- New collection item type request info: ", $data);
-  $this->repository->save($data);
- }
+            Log::info(__METHOD__ . " -- New collection item type request info: ", $newData);
+            $this->repository->save($newData);
+        } catch (Exception $e) {
+            throw new ErrorException(trans('messages.general_error'));
+        }
+    }
 
- public function deleteById($id) {
-  DB::beginTransaction();
+    public function update(CollectionItemType $collectionItemType, array $data)
+    {
+        try {
+            $newData['name'] = $data['name'];
+            $newData['label'] = Arr::exists($data, 'label') ? $data['label'] : null;
+            $collectionItemType = $this->repository->update($newData, $collectionItemType);
 
-  try {
-   Log::info(__METHOD__ . " -- Collection item type data deleted ");
-   $itemType = $this->repository->delete($id);
-  } catch (Exception $e) {
-   DB::rollBack();
-   Log::info($e->getMessage());
-   throw new InvalidArgumentException('Unable to delete collection item type data');
-  }
+        } catch (Exception $e) {
+            Log::info($e->getMessage());
+            throw new ErrorException(trans('messages.general_error'));
+        }
+        return $collectionItemType;
 
-  DB::commit();
-  return $itemType;
- }
-
- public function updateItemType($data, $id) {
-  DB::beginTransaction();
-
-  try {
-   $itemType = $this->repository->update($data, $id);
-  } catch (Exception $e) {
-   DB::rollBack();
-   Log::info($e->getMessage());
-   throw new InvalidArgumentException('Unable to update collection item type data');
-  }
-
-  DB::commit();
-  return $itemType;
- }
+    }
 }

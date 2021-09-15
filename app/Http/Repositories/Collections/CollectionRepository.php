1a6772adf1b4dc5b5f27da242d7e6bb821372bdc
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 13:54:03 +0500

    remove try catch and remove data assignment from repository

--- app/Http/Repositories/Collections/CollectionRepository.php
+++ app/Http/Repositories/Collections/CollectionRepository.php
@@ -3,57 +3,31 @@
 namespace App\Http\Repositories\Collections;
 
 use App\Models\Collection;
-use App\Traits\RemoveImageTrait;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
 
-class CollectionRepository {
- use RemoveImageTrait;
- protected $collection;
- /**
-  * @param array $
-  */
- public function __construct(Collection $collection) {
-  $this->collection = $collection;
+class CollectionRepository
+{
+    protected $collection;
+    /**
+     * @param array $
+     */
+    public function __construct(Collection $collection)
+    {
+        $this->collection = $collection;
+    }
 
- }
+    public function getAll()
+    {
+        return $this->collection->get();
+    }
+    public function save(array $data): void
+    {
+        $this->collection->create($data);
+    }
 
- public function getAll() {
-  return $this->collection->with('user')
-   ->get();
- }
-
- public function getById($id) {
-  return $this->collection
-   ->where('id', $id)->with('user')
-   ->get();
- }
-
- public function save(array $data): void {
-  $collection = new $this->collection;
-  $collection->name = $data['name'];
-  $collection->status = Arr::exists($data, 'status') ? $data['status'] : "Pending";
-  $collection->user_id = Auth::user()->id;
-  $collection->image = Arr::exists($data, 'image_url') ? $data['image_url'] : null;
-  $collection->save();
- }
-
- public function update($data, $id) {
-  $collection = $this->collection->find($id);
-  $collection->name = $data['name'];
-  $collection->status = Arr::exists($data, 'status') ? $data['status'] : "Pending";
-  $collection->user_id = Auth::user()->id;
-  $collection->image = $data['image'];
-  $collection->update();
-  return $collection;
- }
-
- public function delete($id) {
-  $collection = $this->collection->find($id);
-  if ($collection['image']) {
-   $this->removeImage($collection['image']);
-  }
-  $collection->delete();
-  return $collection;
- }
+    public function update(array $data, Collection $collection)
+    {
+        $collection->update($data);
+        return $collection;
+    }
 }

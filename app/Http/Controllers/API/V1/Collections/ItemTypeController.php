0733069576c808a804078a5bf53d4798013881eb
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 14:02:16 +0500

    collection item type refactoring changes in ItemYypeController,  removed try catch and added transformer

--- app/Http/Controllers/API/V1/Collections/ItemTypeController.php
+++ app/Http/Controllers/API/V1/Collections/ItemTypeController.php
@@ -2,130 +2,91 @@
 
 namespace App\Http\Controllers\API\V1\Collections;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
-use App\Models\CollectionItemType;
 use App\Http\Controllers\Controller;
-use App\Http\Services\Collections\ItemTypeService;
 use App\Http\Requests\Collections\ItemTypeSaveRequest;
 use App\Http\Requests\Collections\ItemTypeUpdateRequest;
+use App\Http\Services\Collections\ItemTypeService;
+use App\Http\Transformers\Collections\ItemTypeTransformer;
+use App\Models\CollectionItemType;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
-class ItemTypeController extends Controller {
- /**
-  * Display a listing of the resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
- protected $service;
+class ItemTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected $service;
+    protected $transformer;
 
- public function __construct(ItemTypeService $service) {
-  $this->service = $service;
- }
- public function index(): JsonResponse {
+    public function __construct(ItemTypeService $service, ItemTypeTransformer $transformer)
+    {
+        $this->service = $service;
+        $this->transformer = $transformer;
+    }
 
-  try {
-   $result = $this->service->getAll();
-   if (count($result) == 0) {
-    return $this->success([], null, trans('messages.general_empty_data'));
-   }
-  } catch (Exception $e) {
-   return $this->failure([], null, trans('messages.general_error'));
-  }
+    public function index(): JsonResponse
+    {
+        $result = $this->service->getAll();
+        if (count($result) == 0) {
+            return $this->success($result, $this->transformer, trans('messages.general_empty_data'));
+        }
 
-  return $this->success($result);
- }
+        return $this->success($result, $this->transformer);
+    }
 
- /**
-  * Show the form for creating a new resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
- public function create() {
-  //
- }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ItemTypeSaveRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $result = $this->service->save($data);
+        return $this->success($result, $this->transformer, trans('messages.collection_item_type_create_success'));
 
- /**
-  * Store a newly created resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @return \Illuminate\Http\Response
-  */
- public function store(ItemTypeSaveRequest $request): JsonResponse {
-  $data = $request->only(['name']);
+    }
 
-  try {
-   $this->service->saveItemType($data);
-  } catch (Execption $e) {
-   return $this->failure([], null, trans('messages.general_error'));
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CollectionItemType  $CollectionItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CollectionItemType $collectionItemType)
+    {
+        return $this->success($collectionItemType, $this->transformer);
+    }
 
-  return $this->success([], null, trans('messages.collection_item_type_create_success'));
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CollectionItemType  $CollectionItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ItemTypeUpdateRequest $request, CollectionItemType $collectionItemType)
+    {
+        $data = $request->validated();
+        $result = $this->service->update($collectionItemType, $data);
+        return $this->success($result, $this->transformer, trans('messages.collection_item_type_update_success'));
 
- }
+    }
 
- /**
-  * Display the specified resource.
-  *
-  * @param  \App\Models\CollectionItemType  $CollectionItemType
-  * @return \Illuminate\Http\Response
-  */
- public function show($id) {
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CollectionItemType  $CollectionItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CollectionItemType $collectionItemType)
+    {
+        $collectionItemType->delete();
+        return $this->success([], null, trans('messages.collection_item_type_delete_success'));
 
-  try {
-   $result = $this->service->getById($id);
-   if (count($result) == 0) {
-    return $this->success([], null, trans('messages.general_empty_data'));
-   }
-  } catch (Exception $e) {
-   return $this->failure([], null, trans('messages.general_error'));
-  }
-
-  return $this->success($result);
- }
-
- /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  \App\Models\CollectionItemType  $CollectionItemType
-  * @return \Illuminate\Http\Response
-  */
- public function edit(CollectionItemType $CollectionItemType) {
-  //
- }
-
- /**
-  * Update the specified resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  \App\Models\CollectionItemType  $CollectionItemType
-  * @return \Illuminate\Http\Response
-  */
- public function update(ItemTypeUpdateRequest $request, $id) {
-  $data = $request->only(['name']);
-
-  try {
-   $result = $this->service->updateItemType($data, $id);
-  } catch (Exception $e) {
-   return $this->failure([], null, trans('messages.general_error'));
-  }
-
-  return $this->success([$result], null, trans('messages.collection_item_type_update_success'));
- }
-
- /**
-  * Remove the specified resource from storage.
-  *
-  * @param  \App\Models\CollectionItemType  $CollectionItemType
-  * @return \Illuminate\Http\Response
-  */
- public function destroy($id) {
-
-  try {
-   $result = $this->service->deleteById($id);
-  } catch (Exception $e) {
-   return $this->failure([], null, trans('messages.general_error'));
-  }
-
-  return $this->success([], null, trans('messages.collection_item_type_delete_success'));
- }
+    }
 }

b5ce376a074f86f64bb716b8f2c21e998775ece6
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 19:43:09 +0500

    Data logic moved to service

--- app/Http/Repositories/Sellers/SellerRequestRepository.php
+++ app/Http/Repositories/Sellers/SellerRequestRepository.php
@@ -2,67 +2,27 @@
 
 namespace App\Http\Repositories\Sellers;
 
-use App\Models\Seller;
-use App\Models\User;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
+use App\Models\SellerProfile;
 
 class SellerRequestRepository
 {
-    protected $seller;
+    protected $sellerProfile;
     /**
      * @param array $
      */
-    public function __construct(Seller $seller)
+    public function __construct(SellerProfile $sellerProfile)
     {
-        $this->seller = $seller;
+        $this->sellerProfile = $sellerProfile;
 
     }
 
     public function getAll()
     {
-        return $this->seller->where(['status' => "pending"])->with('user')->get();
-    }
-
-    public function getById($id)
-    {
-        return $this->seller
-            ->where('id', $id)->with('user')
-            ->get();
+        return $this->sellerProfile->where(['status' => "pending"])->with('user')->get();
     }
 
     public function save(array $data): void
     {
-        $seller = new $this->seller;
-        $seller->name = Auth::user()->name;
-        $seller->surname = $data['surname'];
-        $seller->wallet_address = $data['wallet_address'];
-        $seller->physical_address = $data['physical_address'];
-        $seller->p_no = $data['p_no'];
-        $seller->twitter_link = $data['twitter_link'];
-        $seller->insta_link = $data['insta_link'];
-        $seller->twitch_link = $data['twitch_link'];
-        $seller->type = $data['type'];
-        $seller->status = 'Pending';
-        $seller->user_id = Auth::user()->id;
-        $seller->save();
-    }
-
-    public function update($data, $id)
-    {
-        $seller = Seller::find($id);
-        $seller->status = $data['status'];
-        $seller->update();
-        if ($data['status'] == 'Approved') {
-            $user = User::find($seller->user_id);
-            $user->assignRole(['seller']);
-        }
-    }
-
-    public function delete($id)
-    {
-        $seller = $this->seller->find($id);
-        $seller->delete();
-        return $seller;
+        $this->sellerProfile->create($data);
     }
 }

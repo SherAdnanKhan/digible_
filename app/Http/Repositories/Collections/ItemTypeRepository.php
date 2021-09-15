f6880bab92087f370be341b62bbbef1d17106b0a
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 14:04:02 +0500

    remove findbyid and remove data code from repostory and route model binding in case of update

--- app/Http/Repositories/Collections/ItemTypeRepository.php
+++ app/Http/Repositories/Collections/ItemTypeRepository.php
@@ -4,43 +4,33 @@ namespace App\Http\Repositories\Collections;
 
 use App\Models\CollectionItemType;
 
-class ItemTypeRepository {
- protected $collectionItemType;
- /**
-  * @param array $
-  */
- public function __construct(CollectionItemType $collectionItemType) {
-  $this->collectionItemType = $collectionItemType;
+class ItemTypeRepository
+{
+    protected $collectionItemType;
+    /**
+     * @param array $
+     */
+    public function __construct(CollectionItemType $collectionItemType)
+    {
+        $this->collectionItemType = $collectionItemType;
 
- }
+    }
 
- public function getAll() {
-  return $this->collectionItemType
-   ->get();
- }
+    public function getAll()
+    {
+        return $this->collectionItemType
+            ->get();
+    }
 
- public function getById($id) {
-  return $this->collectionItemType
-   ->where('id', $id)
-   ->get();
- }
+    public function save(array $data): void
+    {
+        $this->collectionItemType->create($data);
+    }
 
- public function save(array $data): void {
-  $collectionItemType = new $this->collectionItemType;
-  $collectionItemType->name = $data['name'];
-  $collectionItemType->save();
- }
+    public function update(array $data, CollectionItemType $collectionItemType)
+    {
+        $collectionItemType->update($data);
+        return $collectionItemType;
+    }
 
- public function update($data, $id) {
-  $collectionItemType = $this->collectionItemType->find($id);
-  $collectionItemType->name = $data['name'];
-  $collectionItemType->update();
-  return $collectionItemType;
- }
-
- public function delete($id) {
-  $collectionItemType = $this->collectionItemType->find($id);
-  $collectionItemType->delete();
-  return $collectionItemType;
- }
 }

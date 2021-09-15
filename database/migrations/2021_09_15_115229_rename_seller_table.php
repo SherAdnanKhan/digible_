9d8fd3e668ad4bc5a431ca54f80eb951096f13db
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 19:45:51 +0500

    Seller table renamed to seller_profiles and pno column to phone_no

---
+++ database/migrations/2021_09_15_115229_rename_seller_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameSellerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('sellers', 'seller_profiles');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::rename('seller_profiles' , 'sellers');
+
+    }
+}

9397fd7cb685b0ddc01b92f42e257b4705f39ccf
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 13:51:32 +0500

    changed method name from save,update,findcollecton to save,update,delete. handling try catch and error exception and set newData

--- app/Http/Services/Collections/CollectionService.php
+++ app/Http/Services/Collections/CollectionService.php
@@ -1,62 +1,85 @@
 <?php
 namespace App\Http\Services\Collections;
 
+use App\Exceptions\ErrorException;
 use App\Http\Repositories\Collections\CollectionRepository;
 use App\Http\Services\BaseService;
+use App\Http\Services\Collections\CollectionService;
+use App\Http\Services\Images\ImageDeleteService;
+use App\Http\Services\Images\ImageUploadService;
+use App\Models\Collection;
 use Exception;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use InvalidArgumentException;
 
-class CollectionService extends BaseService {
- protected $repository;
+class CollectionService extends BaseService
+{
+    protected $repository;
+    protected $uploadService;
+    protected $deleteService;
+    protected $service;
 
- public function __construct(CollectionRepository $repository) {
-  $this->repository = $repository;
- }
+    public function __construct(CollectionRepository $repository, ImageUploadService $uploadService, ImageDeleteService $deleteService)
+    {
+        $this->repository = $repository;
+        $this->uploadService = $uploadService;
+        $this->deleteService = $deleteService;
+    }
 
- public function getAll() {
-  Log::info(__METHOD__ . " -- Collection data all fetched: ");
-  return $this->repository->getAll();
- }
+    public function getAll()
+    {
+        Log::info(__METHOD__ . " -- Collection data all fetched: ");
+        return $this->repository->getAll();
+    }
 
- public function getById($id) {
-  Log::info(__METHOD__ . " -- Collection data fetched ");
-  return $this->repository->getById($id);
- }
+    public function save($image, array $data): void
+    {
+        try {
+            $newData['image'] = null;
+            if (isset($data['image'])) {
+                $image = $this->uploadService->uploadImage($image, 'collections');
+                $newData['image'] = $image['image_url'];
+            }
+            $newData['name'] = $data['name'];
+            $newData['status'] = Arr::exists($data, 'status') ? $data['status'] : "Pending";
+            $newData['user_id'] = Auth::user()->id;
 
- public function saveCollection(array $data): void {
-  Log::info(__METHOD__ . " -- New collection request info: ", $data);
-  $this->repository->save($data);
- }
+            Log::info(__METHOD__ . " -- New collection request info: ", $newData);
+            $this->repository->save($newData);
+        } catch (Exception $e) {
+            throw new ErrorException(trans('messages.general_error'));
+        }
+    }
 
- public function deleteById($id) {
-  DB::beginTransaction();
+    public function delete(Collection $collection)
+    {
+        if (isset($collection['image'])) {
+            $this->deleteService->removeImage($collection['image']);
+        }
+        $collection->delete();
+    }
 
-  try {
-   Log::info(__METHOD__ . " -- Collection data deleted ");
-   $collection = $this->repository->delete($id);
-  } catch (Exception $e) {
-   DB::rollBack();
-   Log::info($e->getMessage());
-   throw new InvalidArgumentException('Unable to delete collection data');
-  }
+    public function update(Collection $collection, $image, array $data)
+    {
+        try {
+            if (isset($data['image'])) {
+                if ($collection['image']) {
+                    $this->deleteService->removeImage($collection['image']);
+                }
+                $image = $this->uploadService->uploadImage($image, 'collections');
+                $data['image'] = $image['image_url'];
+            }
+            $newData['name'] = $data['name'];
+            $newData['status'] = Arr::exists($data, 'status') ? $data['status'] : "Pending";
+            $newData['user_id'] = Auth::user()->id;
+            $newData['image'] = Arr::exists($data, 'image') ? $data['image'] : null;
+            $collection = $this->repository->update($newData, $collection);
 
-  DB::commit();
-  return $collection;
- }
-
- public function updateCollection($data, $id) {
-  DB::beginTransaction();
-  try {
-   $collection = $this->repository->update($data, $id);
-  } catch (Exception $e) {
-   DB::rollBack();
-   Log::info($e->getMessage());
-   throw new InvalidArgumentException('Unable to update collection data');
-  }
-
-  DB::commit();
-  return $collection;
- }
+        } catch (Exception $e) {
+            Log::info($e->getMessage());
+            throw new ErrorException(trans('messages.general_error'));
+        }
+        return $collection;
+    }
 }

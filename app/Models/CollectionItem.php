5db4d30d787dcb39b2dfabdf742d2a64749af79f
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-15 16:17:37 +0500

    add status and guarded

--- app/Models/CollectionItem.php
+++ app/Models/CollectionItem.php
@@ -2,18 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class CollectionItem extends Model {
- use HasFactory;
+class CollectionItem extends Model
+{
+    use HasFactory;
+    const STATUS_PENDING = "Pending";
+    const STATUS_APPROVED = "Approved";
+    const STATUS_REJECTED = "Rejected";
 
- public function collection(): BelongsTo {
-  return $this->belongsTo(Collection::class);
- }
+    protected $guarded = ['created_at'];
 
- public function collectionItemType(): BelongsTo {
-  return $this->belongsTo(CollectionItemType::class);
- }
+    public function collection(): BelongsTo
+    {
+        return $this->belongsTo(Collection::class);
+    }
+
+    public function collectionItemType(): BelongsTo
+    {
+        return $this->belongsTo(CollectionItemType::class);
+    }
+    public static function statuses(): array
+    {
+        return [
+            static::STATUS_PENDING => "Pending",
+            static::STATUS_APPROVED => "Approved",
+            static::STATUS_REJECTED => "Rejected",
+        ];
+    }
 }

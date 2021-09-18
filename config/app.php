71773167f45cbe2a552167137b11ae3df6726afb
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-18 15:16:08 +0500

    added intervation config

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -234,6 +235,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\ImageManagerStatic::class,
 
     ],
 

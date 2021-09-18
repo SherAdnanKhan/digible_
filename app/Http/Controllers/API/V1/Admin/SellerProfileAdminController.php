83c946d5e71a0f810efa589703702267b51ed85a
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-18 15:13:07 +0500

    added paginate method for get all and changed request vlaidated

--- app/Http/Controllers/API/V1/Admin/SellerProfileAdminController.php
+++ app/Http/Controllers/API/V1/Admin/SellerProfileAdminController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\API\V1\Admin;
 
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Sellers\UpdateRequest;
 use App\Http\Services\Sellers\SellerRequestService;
 use App\Http\Transformers\Sellers\SellerTransformer;
+use App\Models\SellerProfile;
+use Illuminate\Http\JsonResponse;
 
 class SellerProfileAdminController extends Controller
 {
     protected $service;
 
-    public function __construct(SellerRequestService $service , SellerTransformer $transformer)
+    public function __construct(SellerRequestService $service, SellerTransformer $transformer)
     {
         $this->service = $service;
         $this->transformer = $transformer;
@@ -22,17 +23,12 @@ class SellerProfileAdminController extends Controller
     public function index(): JsonResponse
     {
         $result = $this->service->getAll();
-        if (count($result) == 0) {
-            return $this->success($result, $this->transformer, trans('messages.general_empty_data'));
-        }
-        return $this->success($result, $this->transformer);
+        return $this->service->paginate($result);
     }
 
-    public function update(UpdateRequest $request, $id)
+    public function update(UpdateRequest $request, SellerProfile $sellerProfile): JsonResponse
     {
-
-        $data = $request->only('status');
-        $this->service->update($data, $id);
+        $this->service->update($request->validated(), $sellerProfile);
         return $this->success([], null, trans('messages.seller_request_update_success'));
     }
 }

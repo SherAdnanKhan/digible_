198283d013b741db68a44a88fc036a3d90ec9b5e
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-18 15:08:35 +0500

    Removed extra newdata field, change image to base 64 implement route model binding

--- app/Http/Services/Collections/CollectionItemService.php
+++ app/Http/Services/Collections/CollectionItemService.php
@@ -4,8 +4,8 @@ namespace App\Http\Services\Collections;
 use App\Exceptions\ErrorException;
 use App\Http\Repositories\Collections\CollectionItemRepository;
 use App\Http\Services\BaseService;
-use App\Http\Services\Images\ImageDeleteService;
-use App\Http\Services\Images\ImageUploadService;
+use App\Http\Services\Images\ImageService;
+use App\Models\Collection;
 use App\Models\CollectionItem;
 use Exception;
 use Illuminate\Support\Arr;
@@ -14,44 +14,31 @@ use Illuminate\Support\Facades\Log;
 class CollectionItemService extends BaseService
 {
     protected $repository;
-    protected $uploadService;
-    protected $deleteService;
-    protected $service;
+    protected $imageService;
 
-    public function __construct(CollectionItemRepository $repository, ImageUploadService $uploadService, ImageDeleteService $deleteService)
+    public function __construct(CollectionItemRepository $repository, ImageService $imageService)
     {
         $this->repository = $repository;
-        $this->uploadService = $uploadService;
-        $this->deleteService = $deleteService;
+        $this->imageService = $imageService;
     }
 
-    public function getAll()
+    public function getAll(Collection $collection)
     {
         Log::info(__METHOD__ . " -- Collection item data all fetched: ");
-        return $this->repository->getAll();
+        return $this->repository->getAll($collection);
     }
 
-    public function save($image, array $data): void
+    public function save(array $data, Collection $collection): void
     {
         try {
-            $newData['image'] = null;
             if (isset($data['image'])) {
-                $image = $this->uploadService->uploadImage($image, 'collectionsItem');
-                $newData['image'] = $image['image_url'];
+                $image = $this->imageService->uploadImage($data['image'], 'collectionItems');
+                $data['image'] = $image;
             }
-            $newData['collection_item_type_id'] = $data['collection_item_type_id'];
-            $newData['collection_id'] = $data['collection_id'];
-            $newData['physical'] = Arr::exists($data, 'physical') ? $data['physical'] : false;
-            $newData['name'] = $data['name'];
-            $newData['description'] = Arr::exists($data, 'description') ? $data['description'] : null;
-            $newData['edition'] = Arr::exists($data, 'edition') ? $data['edition'] : null;
-            $newData['graded'] = Arr::exists($data, 'graded') ? $data['graded'] : null;
-            $newData['year'] = Arr::exists($data, 'year') ? $data['year'] : null;
-            $newData['population'] = Arr::exists($data, 'population') ? $data['population'] : null;
-            $newData['publisher'] = Arr::exists($data, 'publisher') ? $data['publisher'] : null;
-            $newData['status'] = Arr::exists($data, 'status') ? $data['status'] : "Pending";
-            Log::info(__METHOD__ . " -- New collection request info: ", $newData);
-            $this->repository->save($newData);
+            $data['status'] = CollectionItem::STATUS_PENDING;
+            $data['nft_type'] = 'non_nft';
+            Log::info(__METHOD__ . " -- New collection request info: ", $data);
+            $this->repository->save($data, $collection);
         } catch (Exception $e) {
             throw new ErrorException(trans('messages.general_error'));
         }
@@ -61,7 +48,7 @@ class CollectionItemService extends BaseService
     {
         try {
             if (isset($collectionItem['image'])) {
-                $this->deleteService->removeImage($collectionItem['image']);
+                $this->imageService->removeImage($collection['image']);
             }
             $collectionItem->delete();
 
@@ -71,30 +58,19 @@ class CollectionItemService extends BaseService
         }
     }
 
-    public function update(CollectionItem $collectionItem, $image, array $data)
+    public function update(CollectionItem $collectionItem, Collection $collection, array $data)
     {
         try {
             if (isset($data['image'])) {
                 if ($collectionItem['image']) {
-                    $this->deleteService->removeImage($collectionItem['image']);
+                    $this->imageService->removeImage($collection['image']);
                 }
-                $image = $this->uploadService->uploadImage($image, 'collectionsItem');
-                $data['image'] = $image['image_url'];
+                $image = $this->imageService->uploadImage($data['image'], 'collectionItems');
+                $data['image'] = $image;
             }
-            $newData['collection_item_type_id'] = $data['collection_item_type_id'];
-            $newData['collection_id'] = $data['collection_id'];
-            $newData['physical'] = Arr::exists($data, 'physical') ? $data['physical'] : false;
-            $newData['name'] = $data['name'];
-            $newData['image'] = Arr::exists($data, 'image') ? $data['image'] : null;
-            $newData['description'] = Arr::exists($data, 'description') ? $data['description'] : null;
-            $newData['edition'] = Arr::exists($data, 'edition') ? $data['edition'] : null;
-            $newData['graded'] = Arr::exists($data, 'graded') ? $data['graded'] : null;
-            $newData['year'] = Arr::exists($data, 'year') ? $data['year'] : null;
-            $newData['population'] = Arr::exists($data, 'population') ? $data['population'] : null;
-            $newData['publisher'] = Arr::exists($data, 'publisher') ? $data['publisher'] : null;
-            $newData['status'] = Arr::exists($data, 'status') ? $data['status'] : "Pending";
-
-            $collectionItem = $this->repository->update($newData, $collectionItem);
+            $data['status'] = Arr::exists($data, 'status') ? $data['status'] : CollectionItem::STATUS_PENDING;
+            $data['nft_type'] = 'non_nft';
+            $collectionItem = $this->repository->update($data, $collectionItem, $collection);
 
         } catch (Exception $e) {
             Log::info($e->getMessage());

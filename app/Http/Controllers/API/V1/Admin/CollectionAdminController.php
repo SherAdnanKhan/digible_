e55debf016bead0bc04a3e2afe24f2d7741edd69
Mohsin <abdul.mohsin.malik@gmail.com>
2021-09-20 18:38:19 +0500

    move paginate to service for collection admin

--- app/Http/Controllers/API/V1/Admin/CollectionAdminController.php
+++ app/Http/Controllers/API/V1/Admin/CollectionAdminController.php
@@ -7,24 +7,23 @@ use App\Http\Requests\Collections\Admin\CollectionUpdateRequest;
 use App\Http\Services\Collections\CollectionService;
 use App\Http\Transformers\Collections\CollectionTransformer;
 use App\Models\Collection;
+use Illuminate\Http\JsonResponse;
 
 class CollectionAdminController extends Controller
 {
     protected $service;
-    protected $transformer;
 
-    public function __construct(CollectionService $service, CollectionTransformer $transformer)
+    public function __construct(CollectionService $service)
     {
         $this->service = $service;
-        $this->transformer = $transformer;
     }
+    
     public function index()
     {
-        $result = $this->service->getPending();
-        return $this->service->paginate($result);
+        return $this->service->getPending();
     }
 
-    public function update(CollectionUpdateRequest $request, Collection $collection)
+    public function update(CollectionUpdateRequest $request, Collection $collection): JsonResponse
     {
         $collection->update($request->validated());
         return $this->success([], null, trans('messages.collection_update_success'));
@@ -32,7 +31,6 @@ class CollectionAdminController extends Controller
 
     public function approved()
     {
-        $result = $this->service->getApproved();
-        return $this->service->paginate($result);
+        return $this->service->getApproved();
     }
 }

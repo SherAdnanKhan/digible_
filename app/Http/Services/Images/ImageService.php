825a0679bdbb55658f99cb7d433722da47b31157
saleh <saleh.khan.developer@gmail.com>
2021-11-23 17:27:06 +0500

    Changed the image dynamically for base64.

--- app/Http/Services/Images/ImageService.php
+++ app/Http/Services/Images/ImageService.php
@@ -1,22 +1,21 @@
 <?php
 namespace App\Http\Services\Images;
 
-use Image;
 use App\Http\Services\BaseService;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
+use Image;
 
 class ImageService extends BaseService
 {
     public function uploadImage($base64, $folderName, $width = 400, $height = 400)
     {
-    $image = str_replace(['data:image/png;base64,','data:image/gif;base64,', 'data:image/jpeg;base64,', 'data:image/jpg;base64,'], '', $base64);
-    $imageName = md5(rand(11111, 99999)) . '_' . time() . '.png';
-    $path =  $imageName;
-    $input = File::put($path, base64_decode($image));
-    $image = Image::make($path)->resize($width, $height);
-    $result = $image->save($path);
-    return $imageName;
+        $image = preg_replace('/^data:image\/[A-z]+;base64,/', "", $base64);
+        $imageName = md5(rand(11111, 99999)) . '_' . time() . '.png';
+        $path = $imageName;
+        $input = File::put($path, base64_decode($image));
+        $image = Image::make($path)->resize($width, $height);
+        $result = $image->save($path);
+        return $imageName;
     }
 
     public function removeImage($image_path)

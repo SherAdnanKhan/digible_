b25c35dcb543a18219bb5661968a25a069bb5f71
saleh <saleh.khan.developer@gmail.com>
2021-11-23 17:13:57 +0500

    Fixed jpeg image error.

--- app/Http/Services/Images/ImageService.php
+++ app/Http/Services/Images/ImageService.php
@@ -10,9 +10,8 @@ class ImageService extends BaseService
 {
     public function uploadImage($base64, $folderName, $width = 400, $height = 400)
     {
-    $image = str_replace('data:image/png;base64,', '', $base64);
-    $image = str_replace(' ', '+', $image);
-    $imageName = md5(rand(11111, 99999)) . '_' . time() . '.jpg';
+    $image = str_replace(['data:image/png;base64,','data:image/gif;base64,', 'data:image/jpeg;base64,', 'data:image/jpg;base64,'], '', $base64);
+    $imageName = md5(rand(11111, 99999)) . '_' . time() . '.png';
     $path =  $imageName;
     $input = File::put($path, base64_decode($image));
     $image = Image::make($path)->resize($width, $height);

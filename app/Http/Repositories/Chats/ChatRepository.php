8b55bdb2d280e0bff4943fd6e4904fea2ae77bfd
saleh <saleh.khan.developer@gmail.com>
2021-11-24 15:36:03 +0500

    Chat message fixed now getting by DESC.

--- app/Http/Repositories/Chats/ChatRepository.php
+++ app/Http/Repositories/Chats/ChatRepository.php
@@ -20,7 +20,7 @@ class ChatRepository
     {
         $chatMessages = ChatMessage::where([['sender_id', auth()->user()->id], ['reciever_id', $user->id]])
             ->orWhere([['sender_id', $user->id], ['reciever_id', auth()->user()->id]])
-            ->orderBy('created_at', 'ASC')->get();
+            ->orderBy('created_at', 'DESC')->get();
         return $chatMessages;
     }
 

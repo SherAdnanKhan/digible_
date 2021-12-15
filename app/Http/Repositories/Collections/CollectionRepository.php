92aba2349f8fdd36130d9a67b77435af363a9a54
saleh <saleh.khan.developer@gmail.com>
2021-12-15 16:21:45 +0500

    Added scrope for approved and rejected collection.

--- app/Http/Repositories/Collections/CollectionRepository.php
+++ app/Http/Repositories/Collections/CollectionRepository.php
@@ -3,28 +3,27 @@
 namespace App\Http\Repositories\Collections;
 
 use App\Models\Collection;
-use Illuminate\Support\Arr;
 
 class CollectionRepository
 {
     public function getAll()
     {
-        return Collection::where(['status' => Collection::STATUS_APPROVED])->with('user')->get();
+        return Collection::approved()->with('user')->get();
     }
 
     public function getCurrentAll()
     {
-        return Collection::where("user_id",auth()->user()->id)->get();
+        return Collection::where("user_id", auth()->user()->id)->get();
     }
 
     public function getPending()
     {
-        return Collection::where(['status' => Collection::STATUS_PENDING])->get();
+        return Collection::rejected()->get();
     }
 
     public function getApproved()
     {
-        return Collection::where(['status' => Collection::STATUS_APPROVED])->get();
+        return Collection::approved()->get();
     }
 
     public function save(array $data): void

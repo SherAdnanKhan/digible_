4c776093d174d4d19ef53ab758fef955a30d1e44
saleh <saleh.khan.developer@gmail.com>
2021-12-27 14:45:22 +0500

    Change data type in collection item update request.

--- app/Http/Requests/Collections/CollectionItemUpdateRequest.php
+++ app/Http/Requests/Collections/CollectionItemUpdateRequest.php
@@ -24,15 +24,15 @@ class CollectionItemUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'collection_item_type_id' => 'required|exists:collection_item_types,id',
-            'name' => 'required|string|max:255',
+            'collection_item_type_id' => 'exists:collection_item_types,id',
+            'name' => 'string|max:255',
             'image' => 'base64img',
             'description' => ['string', 'max:400'],
             'edition' => ['string', 'max:255'],
             'price' => 'numeric|gte:0',
             'graded' => ['string', 'max:255'],
             'year' => 'digits:4|integer|min:1900|max:' . (date('Y') + 1),
-            'population' => ['string', 'max:255'],
+            'population' => ['integer'],
             'publisher' => ['string', 'max:255'],
             'available_for_sale' => 'integer|min:0|digits_between: 0,2',
             'available_at' => 'date_format:Y-m-d H:i:s|after:1 minute',
